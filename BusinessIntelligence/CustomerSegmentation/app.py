f5f15a57370d43bee28dbc460ef1ee0a3afa5153
ManasaR08 <manasar259@gmail.com>
2020-11-07 12:52:35 +0530

    Flask app

---
+++ BusinessIntelligence/CustomerSegmentation/app.py
@@ -0,0 +1,25 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from model import customer_seg
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/upload',methods = ['POST'])  
+def upload():
+    if request.method =='POST':
+        f=request.files['file']
+        f.save(f.filename) 
+        return render_template("success.html",name=f.filename)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)        
