dd4584e66e80c682ce2e2460a85b777c1c19287a
ManasaR08 <manasar259@gmail.com>
2020-11-08 00:47:23 +0530

    Updated

--- BusinessIntelligence/CustomerSegmentation/app.py
+++ BusinessIntelligence/CustomerSegmentation/app.py
@@ -1,8 +1,8 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
-from model import customer_seg
+import os
 
+from flask import Flask, request, render_template, current_app, send_from_directory
+
+from model import customer_seg
 
 app = Flask(__name__)
 
@@ -12,16 +12,19 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/upload',methods = ['POST'])  
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method =='POST':
-        f=request.files['file']
-        f.save(f.filename) 
-        path = f"D:\WUDI_Internship\BusinessIntelligence\CustomerSegmentation\\"
-        customer_seg(path +  f.filename)
-        return render_template("success.html",name=f.filename, value = "result.csv")
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        customer_seg(os.path.join(current_app.root_path, f.filename))
+        return render_template("success.html", name=f.filename)
 
+
+@app.route('/get_result', methods=['GET'])
+def get_result():
+    return send_from_directory(directory=current_app.root_path, filename="result.csv")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)        
+    app.run(debug=True)
