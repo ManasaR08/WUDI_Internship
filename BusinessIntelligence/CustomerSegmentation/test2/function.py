e9783f9167d83bfea4e3f1f3b400c933e4af378c
ManasaR08 <manasar259@gmail.com>
2020-11-16 01:45:27 +0530

    updated

--- BusinessIntelligence/CustomerSegmentation/test2/function.py
+++ BusinessIntelligence/CustomerSegmentation/test2/function.py
@@ -15,40 +15,42 @@ def order_cluster(cluster_field_name, target_field_name,df_,ascending):
 
 def customer_segmentation(path):
     extension = 'xlsx'
+    df_final = pd.DataFrame() 
     files = [i for i in glob.glob('*.{}'.format(extension))]
     for x in files:
         df_all = pd.read_excel(x, encoding='latin-1', sheet_name=None)
         for value in df_all.keys():
             df = df_all[value]
-            df['date'] = pd.to_datetime(df['date'])
-            df_user = pd.DataFrame(df['customer_name'].unique())
-            df_user.columns = ['customer_name']
-            df_max_purchase = df.groupby('customer_name').date.max().reset_index()
-            df_max_purchase.columns = ['customer_name','MaxPurchaseDate']
-            df_max_purchase['recency'] = (df_max_purchase['MaxPurchaseDate'].max() - df_max_purchase['MaxPurchaseDate']).dt.days
-            df_user = pd.merge(df_user, df_max_purchase[['customer_name','recency']], on='customer_name')
-            df_frequency = df.groupby('customer_name').date.count().reset_index()
-            df_frequency.columns = ['customer_name','frequency']
-            df_user = pd.merge(df_user, df_frequency, on='customer_name')
-            df_profit = df.groupby('customer_name').profit.sum().reset_index()
-            df_user = pd.merge(df_user, df_profit, on='customer_name')
-            scaler = MinMaxScaler()
-            df_user[['recency','frequency','profit']] = scaler.fit_transform(df_user[['recency','frequency','profit']])
-            kmeans = KMeans(n_clusters=4)
-            kmeans.fit(df_user[['recency']])
-            df_user['recency_cluster'] = kmeans.predict(df_user[['recency']])
-            df_user = order_cluster('recency_cluster', 'recency',df_user,False)
-            kmeans = KMeans(n_clusters=4)
-            kmeans.fit(df_user[['frequency']])
-            df_user['frequency_cluster'] = kmeans.predict(df_user[['frequency']])
-            df_user = order_cluster('frequency_cluster', 'frequency',df_user,True)
-            kmeans = KMeans(n_clusters=4)
-            kmeans.fit(df_user[['profit']])
-            df_user['profit_cluster'] = kmeans.predict(df_user[['profit']])
-            df_user = order_cluster('profit_cluster', 'profit',df_user,True)
-            df_user['overall_score'] = df_user['recency_cluster'] + df_user['frequency_cluster'] + 1.33*(df_user['profit_cluster'])
-            df_user['segment'] = 'Low-Value'
-            df_user.loc[df_user['overall_score']>2,'segment'] = 'Mid-Value' 
-            df_user.loc[df_user['overall_score']>4,'segment'] = 'High-Value' 
-            df_user.to_csv('customer_segmentation.csv')
-            return
+            df_final = pd.concat([df_final,df]) 
+    df_final['date'] = pd.to_datetime(df_final['date'])
+    df_user = pd.DataFrame(df_final['customer_name'].unique())
+    df_user.columns = ['customer_name']
+    df_max_purchase = df_final.groupby('customer_name').date.max().reset_index()
+    df_max_purchase.columns = ['customer_name','MaxPurchaseDate']
+    df_max_purchase['recency'] = (df_max_purchase['MaxPurchaseDate'].max() - df_max_purchase['MaxPurchaseDate']).dt.days
+    df_user = pd.merge(df_user, df_max_purchase[['customer_name','recency']], on='customer_name')
+    df_frequency = df_final.groupby('customer_name').date.count().reset_index()
+    df_frequency.columns = ['customer_name','frequency']
+    df_user = pd.merge(df_user, df_frequency, on='customer_name')
+    df_profit = df_final.groupby('customer_name').profit.sum().reset_index()
+    df_user = pd.merge(df_user, df_profit, on='customer_name')
+    scaler = MinMaxScaler()
+    df_user[['recency','frequency','profit']] = scaler.fit_transform(df_user[['recency','frequency','profit']])
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(df_user[['recency']])
+    df_user['recency_cluster'] = kmeans.predict(df_user[['recency']])
+    df_user = order_cluster('recency_cluster', 'recency',df_user,False)
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(df_user[['frequency']])
+    df_user['frequency_cluster'] = kmeans.predict(df_user[['frequency']])
+    df_user = order_cluster('frequency_cluster', 'frequency',df_user,True)
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(df_user[['profit']])
+    df_user['profit_cluster'] = kmeans.predict(df_user[['profit']])
+    df_user = order_cluster('profit_cluster', 'profit',df_user,True)
+    df_user['overall_score'] = df_user['recency_cluster'] + df_user['frequency_cluster'] + 1.33*(df_user['profit_cluster'])
+    df_user['segment'] = 'Low-Value'
+    df_user.loc[df_user['overall_score']>2,'segment'] = 'Mid-Value' 
+    df_user.loc[df_user['overall_score']>4,'segment'] = 'High-Value' 
+    df_user.to_csv('customer_segmentation.csv')
+    return
