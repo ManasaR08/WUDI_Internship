bb36a2bc6fe0f6a65ebb3b9b5f7d8e1f0aca824b
ManasaR08 <manasar259@gmail.com>
2020-11-18 00:57:24 +0530

    updated

--- BusinessIntelligence/CreditRisk/test/function.py
+++ BusinessIntelligence/CreditRisk/test/function.py
@@ -7,6 +7,16 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 import os
 
+def order_cluster(cluster_field_name, target_field_name,df_,ascending):
+    new_cluster_field_name = 'new_' + cluster_field_name
+    df_new = df_.groupby(cluster_field_name)[target_field_name].mean().reset_index()
+    df_new = df_new.sort_values(by=target_field_name,ascending=ascending).reset_index(drop=True)
+    df_new['index'] = df_new.index
+    df_final = pd.merge(df_,df_new[[cluster_field_name,'index']], on=cluster_field_name)
+    df_final = df_final.drop([cluster_field_name],axis=1)
+    df_final = df_final.rename(columns={"index":cluster_field_name})
+    return df_final
+
 def credit_risk(path):
     df = pd.read_csv(path)
     df.rename(columns ={'Unnamed: 0': 'new column name'}, inplace = True)
