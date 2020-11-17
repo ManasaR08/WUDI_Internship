28d72233327bfad0e2a26087340c3fcfe22d7feb
ManasaR08 <manasar259@gmail.com>
2020-11-18 00:54:16 +0530

    updated

--- BusinessIntelligence/CreditRisk/test/function.py
+++ BusinessIntelligence/CreditRisk/test/function.py
@@ -1,11 +1,11 @@
 from __future__ import division
 from datetime import datetime, timedelta
 import pandas as pd
-%matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.cluster import KMeans
+import os
 
 def credit_risk(path):
     df = pd.read_csv(path)
@@ -18,58 +18,55 @@ def credit_risk(path):
     df['days'] = -(df.groupby('customer_name')['Date'].diff(periods=-1))
     df['days'] = df['days'].apply(lambda x: x.days)
     df = df.fillna(0)
+    df['CEP'] = df['balance']/df['days']
     df.replace([np.inf, -np.inf], np.nan, inplace=True) 
     df = df.fillna(0)
     df_debit = df.groupby('customer_name')['debit'].sum()
     df_debit.to_csv('df_debit.csv')
-    df_debit = pd.read_csv('df_debit.csv', names=["customer_name", "debit"])
+    df_debit = pd.read_csv('df_debit.csv')
     kmeans = KMeans(n_clusters=4)
     kmeans.fit(df_debit[['debit']])
     df_debit['debitCluster'] = kmeans.predict(df_debit[['debit']])
-    df_count.to_csv('debit_count.csv')
-    df_count = pd.read_csv('debit_count.csv',names=['cluster','count'])
-    k=[]
-    for value in df_count['count']:
-        k.append(value)
-    i=0
-    while(i<=4):
-        if(df_count['count'][i]==max(k)):
-            c = df_count['cluster'][i]
-            break
-        else:
-            i = i+1
-    s = df_count['count'].sum()
-    p = max(df_debit['debitCluster'].value_counts())
-    if p<0.75*s:
-        df_clusters = df_debit
-    else:
-        df_clusters = df_debit[df_debit['debitCluster']!=c]
-        if c=0:
-            df_clusters['debitCluster']=df_clusters['debitCluster'].replace({1:4,2:5,3:6})
-        elif c=1:
-            df_clusters['debitCluster']=df_clusters['debitCluster'].replace({2:5,3:6})
-        elif c=2:
-            df_clusters['debitCluster']=df_clusters['debitCluster'].replace({3:6})
-        df_debitc = df_debit[df_debit['debitCluster']==c]
-        kmeans = KMeans(n_clusters=4)
-        kmeans.fit(df_debitc[['debit']])
-        df_debitc['debitClusterc'] = kmeans.predict(df_debitc[['debit']])
-        if c=1:
-            df_debitc['debitCluster']=df_debitc["debitClusterc"].replace({0:1,1:2,2:3,3:4})
-        elif c=2:
-            df_debitc['debitClusterc']=df_debitc["debitClusterc"].replace({0:2,1:3,2:4,3:5})
-        elif c=3:
-            df_debitc['debitClusterc']=df_debitc["debitClusterc"].replace({0:3,1:4,2:5,3:6})
-        df_debitc.drop(['debitCluster'])
-        df_debitc.rename({'debitClusterc':'debitCluster'})
-        df_clusters = df.concat([df_clusters,df_debitc])
-        df_clusters.to_csv("clusters.csv")
-        os.system('rm debit_count.csv')
-        os.system('rm df_debit.csv')
+    df_debit = order_cluster('debitCluster', 'debit',df_debit,False)
+    df_cep = df.groupby('customer_name')['CEP'].mean()
+    df_cep.to_csv('df_cep.csv')
+    df_cep = pd.read_csv('df_cep.csv')
+    kmeans = KMeans(n_clusters=8)
+    kmeans.fit(df_cep[['CEP']])
+    df_cep['CEPCluster'] = kmeans.predict(df_cep[['CEP']])
+    df_cep = order_cluster('CEPCluster', 'CEP',df_cep,False)
+    df_clusters=df_debit
+    df_clusters['CEP'] = df_cep['CEP']
+    df_clusters['CEPCluster'] = df_cep['CEPCluster']
+    df_purchase = df[df.debit != 0]
+    df_max_purchase = df_purchase.groupby('customer_name').Date.max().reset_index()
+    df_max_purchase.columns = ['customer_name','MaxPurchaseDate']
+    df_max_purchase['Recency'] = (df_max_purchase['MaxPurchaseDate'].max() - df_max_purchase['MaxPurchaseDate']).dt.days
+    df_clusters = pd.merge(df_clusters, df_max_purchase[['customer_name','Recency']], on='customer_name')
+    kmeans = KMeans(n_clusters=8)
+    kmeans.fit(df_clusters[['Recency']])
+    df_clusters['RecencyCluster'] = kmeans.predict(df_clusters[['Recency']])
+    df_clusters = order_cluster('RecencyCluster', 'Recency',df_clusters,False)
+    df_frequency = df_purchase.groupby('customer_name').Date.count().reset_index() #here total purchases are considedred intead of voucher_number based purchases
+    df_frequency.columns = ['customer_name','Frequency']
+    df_clusters = pd.merge(df_clusters, df_frequency, on='customer_name')
+    kmeans = KMeans(n_clusters=8)
+    kmeans.fit(df_clusters[['Frequency']])
+    df_clusters['FrequencyCluster'] = kmeans.predict(df_clusters[['Frequency']])
+    df_clusters = order_cluster('FrequencyCluster', 'Frequency',df_clusters,False)
+    df_clusters['debitCluster']=df_clusters['debitCluster'].replace({0:7,1:6,2:5,3:4,4:3,5:2,6:1,7:0})
+    df_clusters['CEPCluster']=df_clusters['CEPCluster'].replace({0:7,1:6,2:5,3:4,4:3,5:2,6:1,7:0})
+    df_clusters['FrequencyCluster']=df_clusters['FrequencyCluster'].replace({0:7,1:6,2:5,3:4,4:3,5:2,6:1,7:0})
+    df_clusters['overall_score'] = df_clusters['RecencyCluster'] + df_clusters['FrequencyCluster'] + df_clusters['CEPCluster'] +df_clusters['debitCluster']
+    df_clusters['segment'] = 'Low-Value'
+    df_clusters.loc[df_clusters['overall_score']>12,'segment'] = 'Mid-Value' 
+    df_clusters.loc[df_clusters['overall_score']>16,'segment'] = 'High-Value' 
+    df_clusters.to_csv('credit_risk.csv')
+    os.remove("df_debit.csv")
+    os.remove("df_cep.csv")
+    return
 
-return
-
-            
+credit_risk('result.csv')
 
 
 
